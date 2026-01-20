06a1cbcbe925f1cef1dc269c4ef92c4b520004e8
GoDoCity <yellowcabking@msn.com>
2026-01-20 18:00:56 -0500

    Add auth Pages Function

---
+++ functioins/auth.js
@@ -0,0 +1,11 @@
+export async function onRequest(context) {
+  const { env, request } = context;
+  const url = new URL(request.url);
+
+  const redirect = new URL("https://github.com/login/oauth/authorize");
+  redirect.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
+  redirect.searchParams.set("redirect_uri", `${url.origin}/callback`);
+  redirect.searchParams.set("scope", "repo");
+
+  return Response.redirect(redirect.toString(), 302);
+}
