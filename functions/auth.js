91f464f97fd03130e025342a120fc405b80422d1
GoDoCity <yellowcabking@msn.com>
2026-01-20 19:08:53 -0500

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,8 +1,7 @@
-export async function onRequest({ env, request }) {
+export async function onRequest(context) {
+  const { request, env } = context;
   const url = new URL(request.url);
 
-  // Decap may call /auth?provider=github&site_id=...
-  // We always use GitHub, so ignore provider/site_id.
   const redirect = new URL("https://github.com/login/oauth/authorize");
   redirect.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
   redirect.searchParams.set("redirect_uri", `${url.origin}/callback`);
