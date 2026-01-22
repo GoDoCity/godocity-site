f8ff17e90aa3584b1ba00d5bad06d71a394c172f
GoDoCity <yellowcabking@msn.com>
2026-01-21 19:22:23 -0500

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,13 +1,12 @@
 export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
 
+  // Always use the live origin the user is on (prevents undefined SITE_URL)
+  const siteOrigin = url.origin;
+
   const redirect = new URL("https://github.com/login/oauth/authorize");
   redirect.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
-
-  // GitHub MUST send the user back here
-  redirect.searchParams.set("redirect_uri", `${env.SITE_URL}/callback`);
-
-  // keep it simple; repo scope lets Decap write content
+  redirect.searchParams.set("redirect_uri", `${siteOrigin}/callback`);
   redirect.searchParams.set("scope", "repo");
 
   return Response.redirect(redirect.toString(), 302);
