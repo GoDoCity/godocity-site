ae89ad20ef554eb17ff2f5fd8d1c44eedada73ff
GoDoCity <yellowcabking@msn.com>
2026-03-02 19:37:17 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://godocity.com",
   trailingSlash: "always",
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop"
+    }
+  },
 });
